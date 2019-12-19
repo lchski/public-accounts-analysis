d2bc7924c430d105766ced7989742d84c6b8fec6
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-12-19 11:29:04 -0500

    put lword index together

--- load.R
+++ load.R
@@ -11,10 +11,16 @@ professional_services <- tibble(path = fs::dir_ls("data/source/", regexp = "\\.c
     col_types = cols("MINC" = col_double(), "ROBJ_CD" = col_double())
   ) %>%
   filter(! is.na(PRJCT_EN_DESC)) %>% ## remove rows that just total categories, we can do that ourselves
-  remove_extra_columns()
+  remove_extra_columns() %>%
+  mutate(PRJCT_EN_DESC = trimws(PRJCT_EN_DESC, whitespace = "[\\h\\v]"))
 
 pservices_anonymized_vendors <- professional_services %>%
-  filter(str_detect(PRJCT_EN_DESC, "^Service payments under"))
+  filter(str_detect(PRJCT_EN_DESC, regex("^Service payments under", ignore_case = TRUE)))
 
 pservices_named_vendors <- professional_services %>%
-  filter(! str_detect(PRJCT_EN_DESC, "^Service payments under"))
+  filter(! str_detect(PRJCT_EN_DESC, regex("^Service payments under", ignore_case = TRUE)))
+
+
+## to builduse when populating indices (manually, of course!)
+lword <- pservices_named_vendors %>%
+  mutate(lword = word(PRJCT_EN_DESC, -1))
