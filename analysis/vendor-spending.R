a64edf24805dd723b312128598acc3abbcd22fa7
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-12-19 23:11:25 -0500

    generic function to plot spend by vendor by FY

--- analysis/vendor-spending.R
+++ analysis/vendor-spending.R
@@ -1,3 +1,14 @@
+plot_vendor_spend_by_fy <- function(spends_by_vendor = pservices_named_vendors, vendor_to_plot) {
+  spends_by_vendor %>%
+    filter(vendor_normalized == vendor_to_plot) %>%
+    group_by(fyear, ROBJ_EN_NM) %>%
+    summarize(total = sum(AGRG_PYMT_AMT)) %>%
+    ggplot(aes(x = fyear, y = total, fill = ROBJ_EN_NM)) +
+    geom_col()
+}
+
 pservices_named_vendors %>%
-  filter(vendor_normalized == "EY") %>%
-  group_by(fyear, ROBJ_EN_NM) %>% summarize(total = sum(AGRG_PYMT_AMT)) %>% ggplot(aes(x = fyear, y = total, fill = ROBJ_EN_NM)) + geom_col()
+  plot_vendor_spend_by_fy("IBM")
+
+pservices_named_it %>%
+  plot_vendor_spend_by_fy("IBM")
